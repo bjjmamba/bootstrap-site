35a7c82c7810299778cd5678f5a5f9f7e7f22fd4
Artem <furtaa@mail.ru>
2020-03-16 17:45:42 +0500

    Active block добавлен

--- scripts/script.js
+++ scripts/script.js
@@ -18,5 +18,42 @@ function show(event){
 // order
 
 // popup
+let workBlock = document.querySelectorAll(".work-block"),
+    activeImg = document.querySelector(".active-img"),
+    closeBtn = document.querySelector('.close-block'),
+    blockImg = document.querySelector('.block-img');
+
+for(i=0; i < workBlock.length; i++){
+    workBlock[i].addEventListener('mouseover', zoomOver);
+    workBlock[i].addEventListener('mouseout', zoomOut);
+}
+
+
+function zoomOver(){
+    let currentBlock = this,
+        zoomImg = currentBlock.lastElementChild;
+
+    zoomImg.classList.add('zoom-in_active');
+
+    zoomImg.onclick = function() {
+    let currentEl = this,
+        parentEl = currentEl.parentElement.firstElementChild,
+        currentSrc = parentEl.getAttribute('src')
+        activeBlock = document.querySelector('.active-img'); 
+         
+    
+    activeBlock.classList.add('active-block'); 
+    blockImg.setAttribute('src', currentSrc);
+    };
+
+    closeBtn.addEventListener('click', function () {
+        activeBlock.classList.remove('active-block');
+    });
+}
+
+function zoomOut(){
+    currentBlock = document.querySelector(".zoom-in_active");
+    currentBlock.classList.remove('zoom-in_active');
+}
 
 // popup
